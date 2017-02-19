f5df93a20c6d4ba0a8256324e7f3dbb9738fffad
jaga <kandros5591@gmail.com>
2017-02-19 18:28:53 +0100

    add jwt token auth

--- handler.js
+++ handler.js
@@ -6,8 +6,16 @@ module.exports.run = (event, context, callback) => {
     const time = new Date();
     console.log(`Your cron function "${context.functionName}" ran at ${time}`);
     
+    const config = {}
+    
+    if (process.env.JWT_TOKEN) {
+        config.headers = {
+            'Authorization': `Bearer ${process.env.JWT_TOKEN}`
+        }
+    }
+    
     console.log(`calling ${process.env.URL}`)
-    axios.get(process.env.URL)
+    axios.get(process.env.URL, config)
         .then(function () {
             console.log('ok');
             callback(null, 'ok')
